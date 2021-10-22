423fb6ed143cae793867d8d9aa7dc33f651e9682
M.Purushotham Reddy <83862446+puru13794@users.noreply.github.com>
2021-10-22 09:42:21 +0530

    Add files via upload

---
+++ password_decr.rb
@@ -0,0 +1,18 @@
+require 'bcrypt'
+users=[{username: "puru",password: "century"},{username: "rajesh",password: "ramesh"},{username: "suresh",password: "ramesh"}]
+
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+puts create_secure_users(users)
